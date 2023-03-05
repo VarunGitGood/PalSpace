13fcd1ca2f2a4814c5e291060e071cee8341df95
Varun Singh <varun7singh10@gmail.com>
2023-03-05 22:24:19 +0530

    test

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config({ path:"./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Test Successful");
+  res.send("Test Clear");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+app.get("/test", (req, res) => {
+  res.send("Test Clear");
+});
+
+app.listen(process.env.PORT ?? 3000, () => {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  console.log(`Server started on port ${process.env.PORT}`);
 });
