af29c11524ff822b60f69e656be3500933fddd99
Varun Singh <varun7singh10@gmail.com>
2023-03-06 13:55:47 +0530

    fixed linting and formatting

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,23 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config({ path: "./config/config.env" });
-const app = express();
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config({ path: './config/config.env' })
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+const a = 1
 
-app.get("/", (req, res) => {
-  res.send("Test Clear");
-});
+app.use(cors())
+app.use(express.json())
 
-app.get("/test", (req, res) => {
-  res.send("Test Success?");
-});
+app.get('/', (req, res) => {
+  res.send('Test Clear')
+})
 
-app.listen(process.env.PORT ?? 3000, () => {
+app.get('/test', (req, res) => {
+  res.send('Test Success?')
+})
+
+app.listen(process.env.PORT || 3000, () => {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  console.log(`Server started on port ${process.env.PORT}`);
-});
+  console.log(`Server started on port ${process.env.PORT}`)
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import { useState } from 'react';
+function App() {
+  return (
+    <div className="App">
+      <p>hi</p>
+    </div>
+  );
+}
+
+export default App;
