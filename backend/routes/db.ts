1d48c4a3b43b1c00f6a0cbb1bfcfa3c70fca2e80
Varun Singh <varun7singh10@gmail.com>
2023-05-15 22:12:13 +0530

    init

--- backend/routes/db.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-const routes = Router()
-import { joinQueue, flushQueue } from '../controllers/db'
-
-routes.post('/queues/join', joinQueue)
-routes.get('/queues/flush', flushQueue)
-
-export default routes
--- backend/types/request.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express'
-// Socket type not working
-
-interface Req extends Request {
-  socketId: string
-  io: any
-}
-
-export { Req }
