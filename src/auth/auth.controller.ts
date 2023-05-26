13a9e838b93b2c4831754fab683a1ec2c19012fc
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-05-26 11:29:39 +0530

    added guard and googlAuth Data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDto, SignInDto } from './utils/types';
 import { RedisDbService } from 'src/redis-db/redis-db.service';
+import { Request } from 'express';
+import { AuthenticationGuard } from './guards/auth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -17,6 +20,22 @@ export class AuthController {
   @Post('register')
   async register(@Body() userDto: UserDto) {
     return this.authService.register(userDto);
+  }
+
+  @Get('me')
+  @UseGuards(AuthenticationGuard)
+  async me(@Req() req: Request) {
+    return this.authService.me(req);
+  }
+
+  // @Get('google')
+  // @UseGuards(AuthGuard('google'))
+  // async googleAuth(@Req() req: Request) {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.authService.googleLogin(req);
   }
 
   @Get('test')
---
+++ src/auth/utils/google.strategy.ts
@@ -0,0 +1,37 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+
+import { Injectable } from '@nestjs/common';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    // console.log(accessToken, refreshToken, profile);
+
+    const { displayName, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      displayName: displayName,
+      picture: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
