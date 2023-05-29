fafc7cd8968b866c52caff830e84eacf762e04e4
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-05-29 21:33:45 +0530

    swaggerUI and validation of auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,23 +5,29 @@ import { RedisDbService } from 'src/redis-db/redis-db.service';
 import { Request } from 'express';
 import { AuthenticationGuard } from './guards/auth.guard';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(
     private authService: AuthService,
     private redis: RedisDbService,
   ) {}
+  @ApiResponse({ status: 201, description: 'Login Token' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   @Post('login')
   async login(@Body() siginDto: SignInDto) {
     return this.authService.login(siginDto.username, siginDto.password);
   }
 
+  @ApiResponse({ status: 201, description: 'Register Token' })
   @Post('register')
   async register(@Body() userDto: UserDto) {
     return this.authService.register(userDto);
   }
 
+  @ApiBearerAuth()
   @Get('me')
   @UseGuards(AuthenticationGuard)
   async me(@Req() req: Request) {
