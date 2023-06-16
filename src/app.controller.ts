6352a08a1f4f26125b4117054a92fb2155220585
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-06-16 20:26:30 +0530

    rabbitMQ queue setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly rabbitmqService: RabbitmqService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  init(): string {
+    return 'Hello from NestJS';
+  }
+  @Get('produce')
+  async sendQueue(): Promise<string> {
+    this.rabbitmqService.send('RABBIT_SERVICE', {
+      message: 'Hello from NestJS',
+    });
+    return 'Message Sent To queue testqueue';
+  }
+
+  @MessagePattern('RABBIT_SERVICE')
+  public async messageHandler(
+    @Payload() data: any,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    console.log('Data Received from queue testqueue', data);
+    channel.ack(originalMsg);
   }
 }
